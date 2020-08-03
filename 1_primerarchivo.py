7ddfb1b4c288cb417ef712963a32afe18bb96f34
Ezequiel <zekii410@gmail.com>
2020-08-03 19:57:28 -0300

    fusion con rama2

--- 1_primerarchivo.py
+++ 1_primerarchivo.py
@@ -3,8 +3,6 @@ print("hola mundo")
 print("cambios en el camino nuevo")
 print("esto se encuentra ahi")
 print("esto se agrega en un nuevo commit")
-<<<<<<< HEAD
 print("cambios en la master")
-=======
 print("cambios en la rama dos")
->>>>>>> rama2
+
